924b6e2b8f3f6e7bc416d71b9af1428979f952fc
Mahmoud Nasr <m3668115@gmail.com>
2024-01-17 01:07:47 +0200

    create home repos method & failure

---
+++ lib/features/home/data/repos/home_repos.dart
@@ -0,0 +1,9 @@
+// بحدد من خلالها اللي هيتم عندي ف ال  feature
+import 'package:bookly_application/core/errors/failures.dart';
+import 'package:bookly_application/features/home/data/models/BookModel.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepos{
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks ();
+  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks ();
+}
