a4e97a90d2522cdd069ae6830fb41faae33b46ca
Mahmoud Nasr <m3668115@gmail.com>
2024-01-17 22:44:52 +0200

    create states

--- lib/features/home/data/repos/home_repo_implement.dart
+++ lib/features/home/data/repos/home_repo_implement.dart
@@ -36,8 +36,30 @@ class HomeRepoImplement extends HomeRepos {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming',
+      );
+
+      List<BookModel> bookList = [];
+      for (var item in data['items']) {
+        bookList.add(BookModel.fromJson(item));
+      }
+      return right(bookList);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServiceFailure.fromDioException(
+            e,
+          ),
+        );
+      }
+      return left(
+        ServiceFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 }
