edf9295c4d0bce793b1e675ee748f6a39cd1453d
Mahmoud Nasr <m3668115@gmail.com>
2024-01-17 20:52:09 +0200

    create server failure from dioException

--- lib/features/home/data/repos/home_repo_implement.dart
+++ lib/features/home/data/repos/home_repo_implement.dart
@@ -3,6 +3,7 @@ import 'package:bookly_application/core/errors/failures.dart';
 import 'package:bookly_application/features/home/data/models/BookModel.dart';
 import 'package:bookly_application/features/home/data/repos/home_repos.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImplement extends HomeRepos {
   ApiService apiService;
@@ -10,19 +11,27 @@ class HomeRepoImplement extends HomeRepos {
   HomeRepoImplement(this.apiService);
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchNewsBooks () async {
+  Future<Either<Failure, List<BookModel>>> fetchNewsBooks() async {
     try {
       var data = await apiService.get(
           endPoint:
               'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming');
       List<BookModel> bookList = [];
-      for (var item in data ['items'] ) {
+      for (var item in data['items']) {
         bookList.add(BookModel.fromJson(item));
       }
       return right(bookList);
-      
     } catch (e) {
-      return left(ServiceFailure());
+      if (e is DioException) {
+        return left(
+          ServiceFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServiceFailure(
+          e.toString(),
+        ),
+      );
     }
   }
 
